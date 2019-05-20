f0d4e3b9c54e6014f476593bebb7bfa05a9ac8e5
Sylphy0052 <yark0531@yahoo.co.jp>
2019-05-20 20:54:03 +0900

    feat: manipulation user table function

--- InventoryManagementSystem/src/java/Controller/DBController.java
+++ InventoryManagementSystem/src/java/Controller/DBController.java
@@ -2,10 +2,12 @@ package Controller;
 
 import DBModel.StatusTB;
 import DBModel.StorageTB;
+import DBModel.UserTB;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @Stateless
 public class DBController {
@@ -22,6 +24,21 @@ public class DBController {
         em.persist(storage);
     }
     
+    public void add(UserTB user) {
+        em.persist(user);
+    }
+    
+    public UserTB getUser(String name, String pass) {
+        TypedQuery<UserTB> query = em.createQuery(
+                "SELECT u FROM UserTB u WHERE u.name = :name AND u.pass = :pass",
+                UserTB.class
+        );
+        return query
+                .setParameter("name", name)
+                .setParameter("pass", pass)
+                .getSingleResult();
+    }
+    
     public List<StatusTB> getStatusList() {
         return em.createQuery("SELECT s FROM StatusTB s")
                 .getResultList();
@@ -29,6 +46,11 @@ public class DBController {
     
     public List<StorageTB> getStorageList() {
         return em.createQuery("SELECT s FROM StorageTB s")
+                .getResultList();
+    }
+    
+    public List<UserTB> getUserList() {
+        return em.createQuery("SELECT u FROM UserTB u")
                 .getResultList();
     }
 }
