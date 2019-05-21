618af36e7f06f93c701155c4d3875823943eb38e
Sylphy0052 <yark0531@yahoo.co.jp>
2019-05-22 00:55:32 +0900

    fix: don't add multiply

--- InventoryManagementSystem/src/java/BackingBean/TopPage.java
+++ InventoryManagementSystem/src/java/BackingBean/TopPage.java
@@ -20,18 +20,20 @@ import javax.inject.Named;
 public class TopPage implements Serializable {
 
     private String message = "";
+    private boolean flg = true;
 
     @EJB
     private DBController dbc;
 
-    public TopPage() {}
-    
+    public TopPage() {
+    }
+
     @PostConstruct
     public void init() {
         addStatus();
         addStorage();
         addUser();
-        if(message.equals("Add user table")) {
+        if (message.equals("Add user table")) {
             message = "Add Table Initialize";
         } else {
             message = "";
@@ -65,7 +67,7 @@ public class TopPage implements Serializable {
                 message = "Storage table is already added";
             } else {
                 String[] storageNames = {"-Not Select-", "StorageA", "StorageB"};
-                for(String name: storageNames) {
+                for (String name : storageNames) {
                     StorageTB storage = new StorageTB(name);
                     dbc.add(storage);
                 }
@@ -76,7 +78,7 @@ public class TopPage implements Serializable {
         }
         return null;
     }
-    
+
     public String addUser() {
         try {
             List<UserTB> userList = dbc.getUserList();
@@ -91,29 +93,34 @@ public class TopPage implements Serializable {
         }
         return null;
     }
-    
+
     public String addBook() {
-        List<StatusTB> statusList = dbc.getStatusList();
-        List<StorageTB> storageList = dbc.getStorageList();
-        BookTB book1 = new BookTB("PythonによるTCP/IPソケットプログラミング", "小高知宏");
-        InventoryTB inventory1 = new InventoryTB(book1, statusList.get(0), storageList.get(0), "2376", "オーム社", "", "4");
-        BookTB book2 = new BookTB("ゼロから作るDeep Learning", "斎藤康毅");
-        InventoryTB inventory2 = new InventoryTB(book2, statusList.get(0), storageList.get(2), "3672", "オライリー・ジャパン", "持ってる", "8");
-        BookTB book3 = new BookTB("Pythonで体験してわかるアルゴリズムとデータ構造", "西澤弘毅, 森田光");
-        InventoryTB inventory3 = new InventoryTB(book3, statusList.get(3), storageList.get(1), "2592", "近代科学社", "ほしい", "3");
-        BookTB book4 = new BookTB("リーダブルコード", "ダスティン ボズウェル, トレバー フォシェ");
-        InventoryTB inventory4 = new InventoryTB(book4, statusList.get(1), storageList.get(2), "2592", "オライリー・ジャパン", "おすすめ", "120");
-        BookTB book5 = new BookTB("Pythonで動かして学ぶ！あたらしい機械学習の教科書", "伊藤真");
-        InventoryTB inventory5 = new InventoryTB(book5, statusList.get(2), storageList.get(0), "2894", "翔泳社", "", "1");
-        dbc.add(inventory1);
-        dbc.add(inventory2);
-        dbc.add(inventory3);
-        dbc.add(inventory4);
-        dbc.add(inventory5);
-        
+        if (flg) {
+            List<StatusTB> statusList = dbc.getStatusList();
+            List<StorageTB> storageList = dbc.getStorageList();
+            BookTB book1 = new BookTB("PythonによるTCP/IPソケットプログラミング", "小高知宏");
+            InventoryTB inventory1 = new InventoryTB(book1, statusList.get(0), storageList.get(0), "2376", "オーム社", "", "4");
+            BookTB book2 = new BookTB("ゼロから作るDeep Learning", "斎藤康毅");
+            InventoryTB inventory2 = new InventoryTB(book2, statusList.get(0), storageList.get(2), "3672", "オライリー・ジャパン", "持ってる", "8");
+            BookTB book3 = new BookTB("Pythonで体験してわかるアルゴリズムとデータ構造", "西澤弘毅, 森田光");
+            InventoryTB inventory3 = new InventoryTB(book3, statusList.get(3), storageList.get(1), "2592", "近代科学社", "ほしい", "3");
+            BookTB book4 = new BookTB("リーダブルコード", "ダスティン ボズウェル, トレバー フォシェ");
+            InventoryTB inventory4 = new InventoryTB(book4, statusList.get(1), storageList.get(2), "2592", "オライリー・ジャパン", "おすすめ", "120");
+            BookTB book5 = new BookTB("Pythonで動かして学ぶ！あたらしい機械学習の教科書", "伊藤真");
+            InventoryTB inventory5 = new InventoryTB(book5, statusList.get(2), storageList.get(0), "2894", "翔泳社", "", "1");
+            dbc.add(inventory1);
+            dbc.add(inventory2);
+            dbc.add(inventory3);
+            dbc.add(inventory4);
+            dbc.add(inventory5);
+            message = "Add Inventory Table";
+            flg = false;
+        } else {
+            message = "Already Add Inventory Table";
+        }
         return null;
     }
-    
+
     public String clear() {
         message = "";
         return null;
